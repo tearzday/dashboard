18949241302f49e003fa57e6dbc761d205ff08d3
tearzday <biverdaniil@gmail.com>
2025-11-22 15:38:09 +0300

    feat: create header component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,14 @@ import { Table } from '../entities/Table'
 import './styles/index.scss'
 import { TableHeaderOffer } from '../shared/const'
 import { useEffect, useState } from 'react'
-import { CardMetric, Icon, IconVariant, Selector } from '@/shared/ui'
+import { CardMetric, Selector } from '@/shared/ui'
 import Iconnn from '@/shared/assets/platforms/Google Ads.svg';
 import type { MetricsData } from '@/shared/types'
 import { CardInfo } from '@/shared/ui/CardInfo/CardInfo'
 import { Sidebar } from '@/widgets/Sidebar'
 import { StatusBadge } from '@/shared/ui'
 import { StatusBadgeType } from '@/shared/ui/StatusBadge/const'
-import TestIcon from '@/shared/assets/icons/cpa.svg?react'
+import { Header } from '@/widgets/Header'
 
 
 function App() {
@@ -30,8 +30,8 @@ function App() {
   return (
     <div style={{display: 'flex'}}>
       <Sidebar />
-      <Icon icon={TestIcon} variant={IconVariant.CIRCLE_ACCENT} badge/>
-      <div style={{display: 'flex', flexDirection: 'column', gap: '10px', alignItems: 'flex-start', flex: 1, padding: '20px'}}>
+      <div style={{display: 'flex', flexDirection: 'column', gap: '10px', alignItems: 'flex-start', flex: 1,}}>
+              <Header />
         <Selector icon={Iconnn} placeholder='Выберите дату' options={[{label: 'label', value: 'value'}, {label: 'label', value: 'value'}, {label: 'label', value: 'value'}]}/>
         <Table header={TableHeaderOffer} data={offers}/>
         <StatusBadge status={StatusBadgeType.ACTIVE}/>
---
+++ src/widgets/Header/ui/Header/Header.tsx
@@ -0,0 +1,20 @@
+import Avatar from '@/shared/assets/avatar.png'
+import cls from './Header.module.scss'
+import { Icon, IconVariant, Typography, TypographyVariant } from '@/shared/ui'
+import IconSearch from '@/shared/assets/icons/search.svg?react'
+import IconNotification from '@/shared/assets/icons/notification.svg?react'
+
+export const Header = () => {
+  return (
+    <header className={cls.header}>
+      <div className={cls.container}>
+        <img src={Avatar} alt='User avatar'/>
+        <Typography variant={TypographyVariant.H2}>Сергей, добро пожаловать!</Typography>
+      </div>
+      <div className={cls.container}>
+        <Icon icon={IconSearch} variant={IconVariant.CIRCLE_BLACK} />
+        <Icon icon={IconNotification} variant={IconVariant.CIRCLE_BLACK} badge/>
+      </div>
+    </header>
+  )
+}
